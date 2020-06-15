4552d72382fd900658709f90a98a972da497ae13
Alex Foreman <foreman.alexander@gmail.com>
2020-06-15 00:44:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ turns
 end 
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    puts "X"
-    else 
-      puts "O"
+  if turn_count(board) % 2 == 0
+    "X"
+  else 
+    "O"
   end
 end 
